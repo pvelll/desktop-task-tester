abfa5f0c77ace4b81f2730c664729e755b0bd053
Pavel Sushko <nintsao@gmail.com>
2025-03-25 20:09:25 +0300

    initial commit

---
+++ composeApp/src/desktopMain/kotlin/com/sushkpavel/leetcode/main.kt
@@ -0,0 +1,20 @@
+package com.sushkpavel.leetcode
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.sushkpavel.leetcode.presentation.App
+import desktopleetcode.composeapp.generated.resources.Res
+import desktopleetcode.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
+import org.koin.core.KoinApplication
+import org.koin.core.context.startKoin
+
+fun main() = application {
+    startKoin(KoinApplication.init())
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = stringResource(Res.string.app_name),
+    ) {
+        App()
+    }
+}
